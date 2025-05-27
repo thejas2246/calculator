c738679829cf1d2a8e60f8223cf794dc0d7e9d73
Thejas A <thejas2246@gmail.com>
2025-05-27 07:58:13 +0530

    Add basic arithmetic features

--- script.js
+++ script.js
@@ -1,43 +1,90 @@
-function add(a,b){
-    return roundNumber(a+b);
+const operators = document.querySelectorAll(".operator");
+const operand = document.querySelectorAll(".operand");
+const equalSign = document.querySelector(".equals");
+const bigResult = document.querySelector(".big-result");
+bigResult.textContent = 0;
+let leftSide = 0;
+let rightSide = "";
+let middle = "";
+let isOperating = false;
+function add(a, b) {
+  return roundNumber(Number(a) + Number(b));
 }
 
-function subtract(a,b){
-    return roundNumber(a - b)
+function subtract(a, b) {
+  return roundNumber(Number(a) - Number(b));
 }
 
-function multiply(a,b){
-    return roundNumber(a * b)
+function multiply(a, b) {
+  return roundNumber(Number(a) * Number(b));
 }
 
-function divide(a,b){
-    return roundNumber(a/b);
+function divide(a, b) {
+  return roundNumber(Number(a) / Number(b));
+}
+function modulo(a, b) {
+  return roundNumber(Number(a) % Number(b));
 }
 
-function roundNumber(num){
-    return Math.round((num + Number.EPSILON) * 100) / 100
+function roundNumber(num) {
+  return Math.round((num + Number.EPSILON) * 100) / 100;
 }
-console.log(multiply(3,4.3))
-console.log(add(0.1,0.2))
-console.log(subtract(0.1,0.2))
-console.log(divide(2,0))
 
-operate(12,"+",10);
-operate(0.1,"/",0.2)
-function operate(firstNumber,operator,secondNumber){
-    if(operator=="+"){
-        console.log(add(firstNumber,secondNumber));
+function operate(firstNumber, operator, secondNumber) {
+  if (operator == "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator == "-") {
+    return subtract(firstNumber, secondNumber);
+  } else if (operator == "*") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator == "/") {
+    return divide(firstNumber, secondNumber);
+  } else if (operator == "%") {
+    return modulo(firstNumber, secondNumber);
+  } else {
+    return "invalid";
+  }
+}
+
+operators.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let value = e.target.textContent;
+    if (isOperating) {
+      if (rightSide == "") {
+        middle = value;
+      } else {
+        leftSide = operate(leftSide, middle, rightSide);
+        rightSide = "";
+        middle = value;
+      }
+    } else {
+      isOperating = true;
+      middle = value;
     }
-    else if(operator=="-"){
-        console.log(subtract(firstNumber,secondNumber));
+    displayOnScreen(leftSide, middle, rightSide);
+  });
+});
+
+operand.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let value = e.target.textContent;
+    if (!isOperating) {
+      if (leftSide == 0) {
+        leftSide = value;
+      } else {
+        leftSide += value;
+      }
+      displayOnScreen(leftSide, middle, rightSide);
+    } else {
+      rightSide += value;
+      displayOnScreen(leftSide, middle, rightSide);
     }
-    else if(operator=="*"){
-        console.log(multiply(firstNumber,secondNumber));
-    }
-    else if(operator=="/"){
-        console.log(divide(firstNumber,secondNumber));
-    }
-    else{
-        console.log("invalid");
-    }
+  });
+});
+
+equalSign.addEventListener("click", () => {});
+
+function displayOnScreen(firstNumber, operator, secondNumber) {
+  console.log(firstNumber + " " + operator + " " + secondNumber);
+  bigResult.textContent = firstNumber + operator + secondNumber;
 }
